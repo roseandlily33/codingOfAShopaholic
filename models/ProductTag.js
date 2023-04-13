ac85c38063defffeba7a64007477efb08f39b1e1
Victoria Benoit <vrose834@gmail.com>
2023-04-13 14:31:45 -0300

    Finished the associations

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -15,7 +15,7 @@ ProductTag.init(
    product_id: {
     type: DataTypes.INTEGER,
     references: {
-      model: 'reader',
+      model: 'product',
       key: 'id',
     }
    },
--- models/index.js
+++ models/index.js
@@ -4,13 +4,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
 
-// Tags belongToMany Products (through ProductTag)
+})
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
