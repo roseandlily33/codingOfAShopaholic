210d408fb589920bf1699e56202343bdd1069cec
Victoria Benoit <vrose834@gmail.com>
2023-04-14 09:33:33 -0300

    Got the database seeding and fixed the con on serv

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PW, {
+? new Sequelize(process.env.JAWSDB_URL)
+ : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+      decimalNumbers: true,
+     },
+ });
 
 module.exports = sequelize;
